2dbcbb2832d527678f69277556b08ff7b5ccd4b9
smit-18 <66497978+smit-18@users.noreply.github.com>
2020-09-10 17:32:28 +0530

    Update home.js

--- Capstone Project/js/home.js
+++ Capstone Project/js/home.js
@@ -1,4 +1,3 @@
-/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
 function myFunction() {
   var x = document.getElementById("myLinks");
   if (x.style.display === "block") {
@@ -7,3 +6,33 @@ function myFunction() {
     x.style.display = "block";
   }
 }
+
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    document.getElementsByClassName("topnav")[0].style.backgroundColor = "white";
+    var x = document.getElementsByClassName("links");
+    var i;
+    for (i = 0; i < x.length; i++) {
+    x[i].style.color = "black";
+    x[i].style.marginTop = "0vh";
+    x[i].style.lineHeight = "45px"; 
+    }
+
+  } else {
+    document.getElementsByClassName("topnav")[0].style.backgroundColor = "transparent";
+    var x = document.getElementsByClassName("links");
+    var i;
+    for (i = 0; i < x.length; i++) {
+    x[i].style.color = "white";
+    x[i].style.marginTop = "6vh";
+    x[i].style.lineHeight = "0";
+    }
+  } 
+}
+
+
+
